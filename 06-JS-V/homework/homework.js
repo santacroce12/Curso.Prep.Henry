f2f252ef6b5e0f86dec00d9f065e6cbd813da8ce
Juan <lucasantacrocee@gmail.com>
2021-03-25 16:18:03 -0300

    Tareas ralizadas

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -65,17 +65,18 @@ function agregarStringInvertida() {
     return detalle;
   }  
   
-  
-
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  var persona= new Persona(nombre,apellido,edad,dir)
+  return persona;
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  
 }
   
 
